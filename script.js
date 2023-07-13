403e613f876f2b2bd0f56ce24230e15e0a951df1
Inayat-khan <khanetmoi@gmail.com>
2023-07-13 17:08:24 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,104 @@
+const form = document.getElementById("generate-form");
+const qr = document.getElementById("qrcode");
+const logo = document.getElementById("logo")
+const downloadB = document.getElementById("download")
+
+// Button submit
+const onGenerateSubmit = (e) => {
+  e.preventDefault();
+
+  clearUI();
+  const url = document.getElementById("url").value;
+  const size = document.getElementById("size").value;
+
+  // Validate url
+  if (url === "") {
+    alert("Please enter a URL");
+  } else {
+    showSpinner();
+    // Show spinner for 1 sec
+    setTimeout(() => {
+      hideSpinner();
+      generateQRCode(url, size);
+      showScanner();
+      // Generate the save button after the qr code image src is ready
+      setTimeout(() => {
+        // Get save url
+        const saveUrl = qr.querySelector("canvas").toDataURL();
+        // Create save button
+        createSaveBtn(saveUrl);
+      }, 50);
+    }, 10);
+    logo.style.display = "block";
+    downloadB.style.display = "block";
+  }
+};
+
+// Generate QR code
+const generateQRCode = (url, size) => {
+  const qrcode = new QRCode("qrcode", {
+    text: url,
+    width: size,
+    height: size,
+    colorDark : "#0C5FA8",
+	  colorLight : "#f1f1f1",
+    correctLevel : QRCode.CorrectLevel.H
+  });
+};
+
+// Clear QR code and save button
+const clearUI = () => {
+  qr.innerHTML = "";
+  const saveBtn = document.getElementById("save-link");
+  if (saveBtn) {
+    saveBtn.remove();
+  }
+};
+
+// hide  scanner
+const showScanner = () => {
+  const scanner = document.getElementById("qrCodeContainer");
+  scanner.style.display = "block";
+};
+
+// Show spinner
+const showSpinner = () => {
+  const spinner = document.getElementById("spinner");
+  spinner.style.display = "block";
+};
+
+// Hide spinner
+const hideSpinner = () => {
+  const spinner = document.getElementById("spinner");
+  spinner.style.display = "none";
+};
+
+// Create save button to download QR code as image
+const createSaveBtn = (saveUrl) => {
+  const link = document.createElement("a");
+  link.id = "save-link";
+  link.classList =
+    'bg-red-500 hover:bg-red-700 text-white font-bold py-2 rounded w-1/3 m-auto my-5';
+  link.innerHTML = "Save Image";
+
+  link.href = saveUrl;
+  link.download = "qrcode.png";
+
+  document.getElementById("generated").appendChild(link);
+};
+
+hideSpinner();
+
+form.addEventListener("submit", onGenerateSubmit);
+downloadB.addEventListener("click", ()=>{
+  const link = document.createElement("a");
+  link.id = "save-link";
+  link.classList =
+    'bg-red-500 hover:bg-red-700 text-white font-bold py-2 rounded w-1/3 m-auto my-5';
+  link.innerHTML = "Save Image";
+
+  link.href = qr.querySelector("canvas").toDataURL();
+  link.download = "qrcode.png";
+
+  document.getElementById("generated").appendChild(link);
+});
